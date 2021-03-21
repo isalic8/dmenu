0a263d6c6f517b36d7ceb2f826a7d5b4c4cffbeb
p <p>
2021-03-20 23:06:56 -0700

    periodic commit

--- config.h
+++ config.h
@@ -5,13 +5,13 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"TerminessTTF Nerd Font Mono:bold:pixelsize=20:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#F3E2A9", "#000000" },
-	[SchemeSel] = { "#eeeeee", "#2E2E2E" },
+	[SchemeNorm] = { "#a89984", "#282828" },
+	[SchemeSel] = { "#ebdbb2", "#2E2E2E" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
